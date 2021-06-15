333c1ecea00d343f5244d2a27044299253ef53f5
Dylan Muller <md.node.0@gmail.com>
2021-06-15 12:11:52 +0200

    Initial Commit

---
+++ Firmware/Auxiliary MCU/rsa.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void number_rsa_pkcs(unsigned char *str,
+                     int size,
+                     unsigned char *modulus,
+                     unsigned char *output,
+                     int rsa_bits);
+
+int number_rng_set_seed(int seed);
---
+++ README.md
@@ -0,0 +1,6 @@
+# EEE4022F
+
+This repository consists of two sub branches (folders):
+* /Firmware - source code for the auxiliary and USB driver MCU.
+* /Software - source code for the browser extension and native host.
+
